6d78ec06aff8a35ee8feafb5f103813378b67673
reecefowell <reece@codeconsortium.com>
2013-01-17 01:17:19 +0000

    add avatar renderer

--- Component/Provider/SimpleProfileProvider.php
+++ Component/Provider/SimpleProfileProvider.php
@@ -5,14 +5,25 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 class SimpleProfileProvider implements ProfileProviderInterface
 {
+    protected $container;
+
+    public function __construct($container)
+    {
+        $this->container = $container;
+    }
+
     public function transform(UserInterface $user = null)
     {
         $profile = new Profile();
 
+        $asset = $this->container->get('templating.helper.asset');
+
         if (null !== $user) {
             $profile->setUsername($user->getUsername());
+            $profile->setAvatar($asset->getUrl('ccdnforumforum/images/default_avatar/anonymous_avatar.gif'));
         } else {
             $profile->setUsername('Guest');
+            $profile->setAvatar($asset->getUrl('ccdnforumforum/images/default_avatar/anonymous_avatar.gif'));
         }
 
         return $profile;
