a10856d998e9f4f92fdf731764d4b4291931f412
Reece Fowell <reece@codeconsortium.com>
2013-02-17 08:50:52 +0000

    Remove drafts from dashboard

--- Component/Dashboard/DashboardIntegrator.php
+++ Component/Dashboard/DashboardIntegrator.php
@@ -43,7 +43,7 @@ class DashboardIntegrator extends BaseIntegrator implements IntegratorInterface
             'user' => array(
                 'Community' => array(
                     'ccdn_forum_forum_index' => array('name' => 'Forum', 'icon' => $this->basePath . '/bundles/ccdncomponentcommon/images/icons/Black/32x32/32x32_discussion.png'),
-                    'ccdn_forum_forum_draft_list' => array('auth' => 'ROLE_USER', 'name' => 'Forum Drafts', 'icon' => $this->basePath . '/bundles/ccdncomponentcommon/images/icons/Black/32x32/32x32_pen.png'),
+                    //'ccdn_forum_forum_draft_list' => array('auth' => 'ROLE_USER', 'name' => 'Forum Drafts', 'icon' => $this->basePath . '/bundles/ccdncomponentcommon/images/icons/Black/32x32/32x32_pen.png'),
                     'ccdn_forum_forum_subscription_list' => array('auth' => 'ROLE_USER', 'name' => 'Topic Subscriptions', 'icon' => $this->basePath . '/bundles/ccdncomponentcommon/images/icons/Black/32x32/32x32_bookmark.png'),
                 ),
             ),
