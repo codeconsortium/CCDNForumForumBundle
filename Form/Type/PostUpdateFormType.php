d00e7700fe2dbb95bfde97e3dca034b5de6da39f
Reece Fowell <reece@codeconsortium.com>
2013-04-20 13:11:24 +0100

    update form validation

---
+++ Form/Type/PostUpdateFormType.php
@@ -0,0 +1,90 @@
+<?php
+
+/*
+ * This file is part of the CCDNForum ForumBundle
+ *
+ * (c) CCDN (c) CodeConsortium <http://www.codeconsortium.com/>
+ *
+ * Available on github <http://www.github.com/codeconsortium/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CCDNForum\ForumBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+use CCDNForum\ForumBundle\Entity\Post;
+
+/**
+ *
+ * @author Reece Fowell <reece@codeconsortium.com>
+ * @version 1.0
+ */
+class PostUpdateFormType extends AbstractType
+{
+	/**
+	 *
+	 * @access protected
+	 * @var string $postClass
+	 */
+	protected $postClass;
+	
+	/**
+	 *
+	 * @access public
+	 * @var string $postClass
+	 */
+	public function __construct($postClass)
+	{
+		$this->postClass = $postClass;
+	}
+	
+    /**
+     *
+     * @access public
+     * @param FormBuilderInterface $builder, array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+			->add('body', 'bb_editor',
+				array(
+					'label'              => 'ccdn_forum_forum.form.label.post.body',
+					'translation_domain' => 'CCDNForumForumBundle'
+		        )
+			)
+		;
+    }
+
+    /**
+     *
+     * @access public
+     * @param array $options
+	 * @return array
+     */
+    public function getDefaultOptions(array $options)
+    {
+        return array(
+            'data_class'          => $this->postClass,
+            'csrf_protection'     => true,
+            'csrf_field_name'     => '_token',
+            // a unique key to help generate the secret token
+            'intention'           => 'post_item',
+            'validation_groups'   => array('post_update'),
+            'cascade_validation'  => true,
+        );
+    }
+
+    /**
+     *
+     * @access public
+     * @return string
+     */
+    public function getName()
+    {
+        return 'Post';
+    }
+}
---
+++ Form/Type/TopicCreateFormType.php
@@ -0,0 +1,96 @@
+<?php
+
+/*
+ * This file is part of the CCDNForum ForumBundle
+ *
+ * (c) CCDN (c) CodeConsortium <http://www.codeconsortium.com/>
+ *
+ * Available on github <http://www.github.com/codeconsortium/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CCDNForum\ForumBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ *
+ * @author Reece Fowell <reece@codeconsortium.com>
+ * @version 1.0
+ */
+class TopicCreateFormType extends AbstractType
+{
+	/**
+	 *
+	 * @access protected
+	 * @var string $topicClass
+	 */
+	protected $topicClass;
+	
+	/**
+	 *
+	 * @access public
+	 * @var string $topicClass
+	 */
+	public function __construct($topicClass)
+	{
+		$this->topicClass = $topicClass;
+	}
+	
+    /**
+     *
+     * @access public
+     * @param FormBuilderInterface $builder, array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+			->add('board', 'entity',
+				array(
+		            'property'           => 'name',
+		            'class'              => 'CCDNForumForumBundle:Board',
+		            'query_builder'      => function($qb) { return $qb->createQueryBuilder('b')->orderBy('b.id', 'ASC'); },
+		            'label'              => 'ccdn_forum_forum.form.label.board',
+					'translation_domain' =>  'CCDNForumForumBundle',
+		        )
+			)
+			->add('title', null,
+				array(
+		        	'label'              => 'ccdn_forum_forum.form.label.topic.title',
+					'translation_domain' => 'CCDNForumForumBundle'
+		        )
+			)
+		;
+    }
+
+    /**
+     *
+     * @access public
+     * @param array $options
+	 * @return array
+     */
+    public function getDefaultOptions(array $options)
+    {
+        return array(
+            'data_class'         => 'CCDNForum\ForumBundle\Entity\Topic',
+            'csrf_protection'    => true,
+            'csrf_field_name'    => '_token',
+            // a unique key to help generate the secret token
+            'intention'          => 'topic_item',
+            'validation_groups'  => array('topic_create'),
+        );
+    }
+
+    /**
+     *
+     * @access public
+     * @return string
+     */
+    public function getName()
+    {
+        return 'Topic';
+    }
+}
