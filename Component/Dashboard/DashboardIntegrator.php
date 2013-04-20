79e6729f418a9e33bd1a7c844c15e13663f9a049
Reece Fowell <reece@codeconsortium.com>
2013-04-20 08:35:51 +0100

    rewrite dashboard integrators

--- Component/Dashboard/DashboardIntegrator.php
+++ Component/Dashboard/DashboardIntegrator.php
@@ -13,44 +13,50 @@
 
 namespace CCDNForum\ForumBundle\Component\Dashboard;
 
-use CCDNComponent\DashboardBundle\Component\Integrator\BaseIntegrator;
-use CCDNComponent\DashboardBundle\Component\Integrator\IntegratorInterface;
+use CCDNComponent\DashboardBundle\Component\Integrator\Model\BuilderInterface;
 
 /**
  *
  * @author Reece Fowell <reece@codeconsortium.com>
- * @version 1.0
+ * @version 2.0
  */
-class DashboardIntegrator extends BaseIntegrator implements IntegratorInterface
+class DashboardIntegrator
 {
-
     /**
-     *
-     * Structure of $resources
-     * 	[DASHBOARD_PAGE <string>]
-     * 		[CATEGORY_NAME <string>]
-     *			[ROUTE_FOR_LINK <string>]
-     *				[AUTH <string>] (optional)
-     *				[URL_LINK <string>]
-     *				[URL_NAME <string>]
 	 * 
 	 * @access public
-	 * @return array $resources
+     * @param CCDNComponent\DashboardBundle\Component\Integrator\Model\BuilderInterface $builder
      */
-    public function getResources()
+    public function build(BuilderInterface $builder)
     {
-        $resources = array(
-            'user' => array(
-                'Community' => array(
-                    'ccdn_forum_forum_index' => array('name' => 'Forum', 'icon' => $this->basePath . '/bundles/ccdncomponentcommon/images/icons/Black/32x32/32x32_discussion.png'),
-                    //'ccdn_forum_forum_draft_list' => array('auth' => 'ROLE_USER', 'name' => 'Forum Drafts', 'icon' => $this->basePath . '/bundles/ccdncomponentcommon/images/icons/Black/32x32/32x32_pen.png'),
-                    'ccdn_forum_forum_subscription_list' => array('auth' => 'ROLE_USER', 'name' => 'Topic Subscriptions', 'icon' => $this->basePath . '/bundles/ccdncomponentcommon/images/icons/Black/32x32/32x32_bookmark.png'),
-                ),
-            ),
-
-        );
-
-        return $resources;
+		$builder
+			->addCategory('community')
+				->setLabel('ccdn_forum_forum.dashboard.categories.forum', array(), 'CCDNForumForumBundle')
+				->addPages()
+					->addPage('forum')
+						->setLabel('ccdn_forum_forum.dashboard.pages.forum', array(), 'CCDNForumForumBundle')
+					->end()
+				->end()
+				->addLinks()	
+					->addLink('forum_index')
+						->setRoute('ccdn_forum_forum_index')
+						->setIcon('/bundles/ccdncomponentcommon/images/icons/Black/32x32/32x32_discussion.png')
+						->setLabel('ccdn_forum_forum.title.category.index', array(), 'CCDNForumForumBundle')
+					->end()
+					->addLink('forum_drafts')
+						->setAuthRole('ROLE_USER')
+						->setRoute('ccdn_forum_forum_draft_list')
+						->setIcon('/bundles/ccdncomponentcommon/images/icons/Black/32x32/32x32_pen.png')
+						->setLabel('ccdn_forum_forum.title.drafts.show', array(), 'CCDNForumForumBundle')
+					->end()
+					->addLink('forum_subscriptions')
+						->setAuthRole('ROLE_USER')
+						->setRoute('ccdn_forum_forum_subscription_list')
+						->setIcon('/bundles/ccdncomponentcommon/images/icons/Black/32x32/32x32_bookmark.png')
+						->setLabel('ccdn_forum_forum.title.subscriptions.show', array(), 'CCDNForumForumBundle')
+					->end()
+				->end()
+			->end()
+		;
     }
-
 }
