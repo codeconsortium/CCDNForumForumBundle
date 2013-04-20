a7168428144e02b7e6f5d853d013b273fa178c97
Reece Fowell <reece@codeconsortium.com>
2013-04-20 13:18:51 +0100

    remove unused repo methods

--- Repository/DraftRepository.php
+++ Repository/DraftRepository.php
@@ -15,49 +15,5 @@ use Pagerfanta\Pagerfanta;
  */
 class DraftRepository extends EntityRepository
 {
-//    /**
-//     *
-//     * @access public
-//     * @param  int $userId
-//     * @return Collection
-//     */
-//    public function findDraftsPaginated($userId)
-//    {
-//        $draftsQuery = $this->getEntityManager()
-//            ->createQuery('
-//                SELECT d
-//                FROM CCDNForum\ForumBundle\Entity\Draft d
-//                WHERE d.createdBy = :id
-//                ORDER BY d.createdDate ASC
-//                ')
-//            ->setParameter('id', $userId);
-//
-//        try {
-//            return new Pagerfanta(new DoctrineORMAdapter($draftsQuery));
-//        } catch (\Doctrine\ORM\NoResultException $e) {
-//            return null;
-//        }
-//    }
-//
-//    /**
-//     *
-//     * @access public
-//     * @param  int $draftId, int $userId
-//     * @return null|Draft
-//     */
-//    public function findOneByIdForUserById($draftId, $userId)
-//    {
-//        $query = $this->getEntityManager()
-//            ->createQuery('
-//                SELECT d
-//                FROM CCDNForum\ForumBundle\Entity\Draft d
-//                WHERE d.id = :draftId AND d.createdBy = :userId')
-//            ->setParameters(array('draftId' => $draftId, 'userId' => $userId));
-//
-//        try {
-//            return $query->getsingleResult();
-//        } catch (\Doctrine\ORM\NoResultException $e) {
-//            return null;
-//        }
-//    }
+
 }
--- Repository/RegistryRepository.php
+++ Repository/RegistryRepository.php
@@ -25,47 +25,5 @@ use Doctrine\ORM\EntityRepository;
  */
 class RegistryRepository extends EntityRepository
 {
-//    /**
-//     *
-//     * @access public
-//     * @param int $userId
-//     */
-//    public function findRegistryRecordForUser($userId)
-//    {
-//        $query = $this->getEntityManager()
-//            ->createQuery('
-//                SELECT r
-//                FROM CCDNForum\ForumBundle\Entity\Registry r
-//                WHERE r.ownedBy = :id
-//                ')
-//            ->setParameter('id', $userId);
-//
-//        try {
-//            return $query->getSingleResult();
-//        } catch (\Doctrine\ORM\NoResultException $e) {
-//            return;
-//        }
-//    }
-//
-//    /**
-//     *
-//     * @access public
-//	 * @param array $registryUserIds
-//     */
-//    public function getPostCountsForUsers($registryUserIds)
-//    {
-//        $qb = $this->getEntityManager()->createQueryBuilder();
-//        $query = $qb
-//            ->add('select', 'r')
-//            ->from('CCDNForum\ForumBundle\Entity\Registry', 'r')
-//            ->where($qb->expr()->in('r.ownedBy', '?1'))
-//            ->setParameters(array('1' => array_values($registryUserIds)))
-//            ->getQuery();
-//
-//        try {
-//            return $query->getResult();
-//        } catch (\Doctrine\ORM\NoResultException $e) {
-//            return null;
-//        }
-//    }
+
 }
