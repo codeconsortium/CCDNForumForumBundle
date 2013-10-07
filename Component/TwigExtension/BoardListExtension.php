aed2ddf667cdbf8876fd3f864a042c5fb58a4810
Reece Fowell <reece@codeconsortium.com>
2013-10-07 20:47:48 +0100

    setup usable out of the box Bootstrap 3 layout and theme

--- Component/TwigExtension/BoardListExtension.php
+++ Component/TwigExtension/BoardListExtension.php
@@ -65,9 +65,11 @@ class BoardListExtension extends \Twig_Extension
      */
     public function boardList($forumName)
     {
-        //return $this->categoryModel->findAllBoardsGroupedByCategory();
-        //return $this->categoryModel->findAllCategoriesWithBoardsForForumByName($forumName);
-        return null;
+		if (!$forumName) {
+			$forumName = 'default';
+		}
+		
+        return $this->categoryModel->findAllCategoriesWithBoardsForForumByName($forumName);
     }
 
     /**
