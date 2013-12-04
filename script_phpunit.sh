10513f19d19817c617a999ce15144dbcdbb86738
Reece Fowell <reece@codeconsortium.com>
2013-12-04 16:59:49 +0000

    minor fixes

--- script_phpunit.sh
+++ script_phpunit.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env sh
 
+rm -rRf ./Tests/Functional/app/cache/*
+rm -rRf ./Tests/Functional/app/logs/*
+
+composer install --dev
+
 php ./Tests/Functional/app/console --env=test doctrine:database:drop --force
 php ./Tests/Functional/app/console --env=test doctrine:database:create
+php ./Tests/Functional/app/console --env=test doctrine:schema:create
 php ./Tests/Functional/app/console --env=test doctrine:schema:update --force
 	
 phpunit -c ./ --testdox
