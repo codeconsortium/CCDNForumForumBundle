8299a2440983a21a44b223d69e043d733b6fca1e
Reece Fowell <reece@codeconsortium.com>
2013-12-27 10:07:58 +0000

    separate config parameters for use on local tests and travis

--- script_behat.sh
+++ script_behat.sh
@@ -3,6 +3,18 @@
 rm -rRf ./Tests/Functional/app/cache/*
 rm -rRf ./Tests/Functional/app/logs/*
 
+rm -f ./Tests/Functional/app/config/parameters.yml
+echo 'parameters:
+    database_driver: pdo_mysql
+    database_host: 127.0.0.1
+    database_port: null
+    database_name: ccdn_test
+    database_user: ccdnroot
+    database_password: root
+    locale: en
+
+' > ./Tests/Functional/app/config/parameters.yml
+
 composer install --dev
 
 php ./Tests/Functional/app/console --env=test doctrine:database:drop --force
--- script_phpunit.sh
+++ script_phpunit.sh
@@ -3,11 +3,23 @@
 rm -rRf ./Tests/Functional/app/cache/*
 rm -rRf ./Tests/Functional/app/logs/*
 
+rm -f ./Tests/Functional/app/config/parameters.yml
+echo 'parameters:
+    database_driver: pdo_mysql
+    database_host: 127.0.0.1
+    database_port: null
+    database_name: ccdn_test
+    database_user: ccdnroot
+    database_password: root
+    locale: en
+
+' > ./Tests/Functional/app/config/parameters.yml
+
 composer install --dev
 
 php ./Tests/Functional/app/console --env=test doctrine:database:drop --force
 php ./Tests/Functional/app/console --env=test doctrine:database:create
 php ./Tests/Functional/app/console --env=test doctrine:schema:create
 php ./Tests/Functional/app/console --env=test doctrine:schema:update --force
-	
+
 phpunit -c ./ --testdox
