656d12a13cafa0b7561ccc932e80fe937fa3f5c9
Jonas Dambacher <jonas.dambacher@coffee-bike.com>
2017-08-30 16:32:08 +0200

    Fixed use error

--- Controller/BaseController.php
+++ Controller/BaseController.php
@@ -13,7 +13,7 @@
 
 namespace CCDNForum\ForumBundle\Controller;
 
-use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
